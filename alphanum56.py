1c826a3a3dba317c4f3117c66c6afa39a03867ba
Brindha <51919498+Brindha27@users.noreply.github.com>
2019-06-25 14:04:36 +0530

    Create alphanum56.py

---
+++ alphanum56.py
@@ -0,0 +1,13 @@
+bri=str(input())
+k=0
+l=0
+m=len(bri)
+for i in range(0,m):
+  if(bri[i].isalpha()):
+    k=k+1
+  if(bri[i].isnumeric()):
+    l=l+1
+if((k==0)or(l==0))
+  print("No")
+else:
+  print("Yes")
