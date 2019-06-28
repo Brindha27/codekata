4edd97f3ffc7f25aca364a2c909bc7954af88878
Brindha <51919498+Brindha27@users.noreply.github.com>
2019-06-28 16:40:23 +0530

    Update prefix66.py

--- prefix66.py
+++ prefix66.py
@@ -1,11 +1,9 @@
-k=int(int(input())
-l=list(map(str,input().split()))
-m=list(input())
-b=0
-for i in range(K):
-  x=(list(l[i])
-  for j in range(0,len(x)-len(m)+1):
-    if m==x[j:len(m)]:
-      b=b+1
-      break
-print(b)
+k=input()
+m=input().split()
+x=0
+y=input()
+for i in m:
+  if y==i[:len(y)]:
+    x+=1
+print(x)
+    
